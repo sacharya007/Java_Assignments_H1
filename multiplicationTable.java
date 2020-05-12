574541663d0090240f7530064ba4577d2cbd43e2
shreya <shreya.acharya7@gmail.com>
2020-05-12 17:26:01 -0500

    first commit

---
+++ multiplicationTable.java
@@ -0,0 +1,11 @@
+public class multiplicationTable{
+
+	 public static void main(String args[])
+	{
+	  int num = 5;
+	  for(int i=1;i<=10;i++)
+	{
+	  System.out.println("The product of " +num+ " X " +i+ " = " +(num*i));
+	}
+}
+}
